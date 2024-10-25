1ecebeae1c7a065ba7fb954ae76caa302c444d73
jacopaghera <123456789+jacopaghera@gmail.com>
2024-10-25 21:10:15 +0200

    removed the debugging p

--- bubblesort.rb
+++ bubblesort.rb
@@ -8,18 +8,14 @@ def bubble_sort(array)
     last_number = 0
     last_index = 0
     array.each_with_index do |n, idx| #iteration
-        p last_number
         if n < last_number
           array[last_index], array[idx] = array[idx], array[last_index]
         end
         last_number = n
         last_index = idx
-        p last_number
       end
     flag += 1 #augment flag until one before the end
-    p flag
   end
-  p array
 end
 
 bubble_sort([7, 2, 5, 9, 3, 10, 15, 14])
